6d83468fba50ccfb20253dcc1b9d2db3504b67a5
Tomasz Jonak <tcjonak@gmail.com>
2016-07-19 14:33:08 +0200

    fix

--- spark.py
+++ spark.py
@@ -16,14 +16,15 @@ floatFile = splitFile.map(lambda w: [float(x) for x in w[0:-1]] + [w[-1]])
 versicolor = floatFile.filter(lambda row: row[-1] == "Iris-versicolor")
 
 def cartesian_distance(a, b):
-    distances = [(z - y)**2 for z, y in zip(a, b)]
+    distances = [(x - y)**2 for x, y in zip(a, b)]
     return math.sqrt(sum(distances))
 
 # print(cartesian_distance([3, 4], [0,0]))
 
 
 pruned = versicolor.map(lambda row: row[0:-1])
-sample = pruned.take(1)
+sample = pruned.take(1)[0]
+# print(sample)
 distances = pruned.map(lambda row: cartesian_distance(row, sample))
 # pprint(pruned.collect())
 pprint(distances.collect())
