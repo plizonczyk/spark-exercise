08a025be32adeb5bc934cd5de402bff45331ba02
Tomasz Jonak <tcjonak@gmail.com>
2016-07-19 13:48:44 +0200

    slucham psa jak gra

--- spark.py
+++ spark.py
@@ -4,8 +4,13 @@ findspark.init()
 import pyspark
 sc = pyspark.SparkContext()
 
-textFile = sc.textFile("data.txt")
-print(textFile.count())
+from pprint import pprint
 
-linesWithSpark = textFile.filter(lambda line: "third" in line)
-linesWithSpark.count()
+textFile = sc.textFile("data.txt")
+splitFile = textFile.map(lambda w: w.split('\t'))
+filtered = splitFile.filter(lambda row: row[-1] == "Iris-versicolor")
+pprint(filtered.collect())
+# print(textFile.count())
+
+# linesWithSpark = textFile.filter(lambda line: "third" in line)
+# linesWithSpark.count()
