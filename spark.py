037ba57c199c8f817edd3c9993abd812e9d26741
Tomasz Jonak <tcjonak@gmail.com>
2016-07-19 14:29:50 +0200

    kek

--- spark.py
+++ spark.py
@@ -5,12 +5,31 @@ import pyspark
 sc = pyspark.SparkContext()
 
 from pprint import pprint
+from matplotlib import pyplot
+
+import math
 
 textFile = sc.textFile("data.txt")
 splitFile = textFile.map(lambda w: w.split('\t'))
-filtered = splitFile.filter(lambda row: row[-1] == "Iris-versicolor")
-pprint(filtered.collect())
+floatFile = splitFile.map(lambda w: [float(x) for x in w[0:-1]] + [w[-1]])
+# pprint(floatFile.collect())
+versicolor = floatFile.filter(lambda row: row[-1] == "Iris-versicolor")
+
+def cartesian_distance(a, b):
+    distances = [(z - y)**2 for z, y in zip(a, b)]
+    return math.sqrt(sum(distances))
+
+# print(cartesian_distance([3, 4], [0,0]))
+
+
+pruned = versicolor.map(lambda row: row[0:-1])
+sample = pruned.take(1)
+distances = pruned.map(lambda row: cartesian_distance(row, sample))
+# pprint(pruned.collect())
+pprint(distances.collect())
+
 # print(textFile.count())
+# pprint(filtered.collect())
 
 # linesWithSpark = textFile.filter(lambda line: "third" in line)
 # linesWithSpark.count()
