170c5b3bbf1fe6b7c16fee2e594f96f4dadff4f5
Tomasz Jonak <tcjonak@gmail.com>
2016-07-21 10:10:44 +0200

    Main loop, not finished

--- kmeans_api.py
+++ kmeans_api.py
@@ -7,11 +7,13 @@ class KMeans:
     def __init__(self):
         self.k = 0
         self.runs = 0
-        self.max_iterations = None
+        self.max_iterations = 0
         # self.epsilon = epsilon
 
-    def train(self, rdd, runs):
+    def train(self, rdd, k, runs, max_iterations):
         self.runs = runs
+        self.max_iterations = max_iterations
+        self.k = k
         self._run(rdd)
 
     def _run(self, rdd):
@@ -27,18 +29,57 @@ class KMeans:
         norms.unpersist()
 
     def _run_algorithm(self, rdd):
-        sc = rdd.SparkContext
+        sc = rdd.context
 
-        if not self.runs or not self.k:
+        if not self.runs or not self.k or not self.max_iterations:
             raise RuntimeError("0 value is not valid for params")
 
         # TODO: explore other init methods
-        self._random_init(rdd)
+        centers = self._random_init(rdd)
+
+        active = [True for _ in range(self.runs)]
+        costs = [0 for _ in range(self.runs)]
+
+        active_runs = [0 for _ in range(self.runs)]
+        iteration = 0
+        pprint(active_runs)
+        # while iteration < self.max_iterations and active_runs:
+
+        active_centers = list(map(lambda w: centers[w], active_runs))
+        cost_accumulators = list(map(lambda _: sc.accumulator(0.0), active_runs))
+        pprint(active_centers)
+        pprint(cost_accumulators)
+        # broadcast_active_centers = sc.broadcast(active_centers)
+
+        # def fetch_sum_and_count_for_each_center(points):
+        #     this_active_centers = broadcast_active_centers.value
+        #     runs = len(this_active_centers)
+        #     k = len(this_active_centers[0])
+        #     dims = len(this_active_centers[0][0].vec)
+        #
+        #     sums = [[[0 for _ in range(dims)] for _ in range(k)] for _ in range(runs)]
+        #     counts = [[0 for _ in range(k)] for _ in range(runs)]
+        #
+        #     def populate(point):
+        #         for i in range(runs):
+        #             active_centers = this_active_centers(i)
+        #             (best_center, cost) = find_closest(active_centers, point)
+        #             cost_accumulators[i] += cost
+        #             # the hell are 2 lines below for
+        #             sum = sums[i][best_center]
+        #             point.vec = [x + y for x, y in zip(point.vec, sum)]
+        #             counts[i][best_center] += 1
+        #
+        #     points.foreach(populate)
+
+        # total_contributions = rdd.mapPartitions(fetch_sum_and_count_for_each_center)
 
     def _random_init(self, rdd):
         # Spark uses xorbitshift, im lazy so default randomness source
         sample = rdd.takeSample(True, self.runs * self.k)
 
+        inits = [sample[r * self.k:(r+1) * self.k] for r in range(self.runs)]
+        return inits
 
 
 def main():
@@ -59,7 +100,7 @@ def main():
     floatRDD = splitRDD.map(lambda w: [float(x) for x in w[0:-1]])
 
     means = KMeans()
-    means.train(floatRDD)
+    means.train(floatRDD, k=3, runs=10, max_iterations=10)
 
     # KMeans(floatRDD, 3, max_iterations=10, runs=10)
 
