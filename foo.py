74b049909d98b44790c3b47cb5c26573c1471020
Tomasz Jonak <tcjonak@gmail.com>
2016-08-05 12:09:36 +0200

    mean calculation fix

--- foo.py
+++ foo.py
@@ -1,7 +1,7 @@
 import pprint as pp
 import functools as ft
 import random
-from utils import euclidean_norm
+from utils import euclidean_distance
 
 
 def main():
@@ -43,31 +43,68 @@ def main():
     dimReducedRdd = floatRdd.map(lambda a: [a[i] for i in range(len(a)) if i in workingDims])
     print('Reduced rowsize:', len(dimReducedRdd.first()))
 
-    # map to scalars
-    meansRDD = dimReducedRdd.map(euclidean_norm)
-    print('Records:', meansRDD.count())
-
-    # take random seeds mean, should be reworked to use them concurrently instead
+    # amount of runs
     quantity = 10
-    sample = meansRDD.takeSample(False, quantity)
+    # take random seeds
+    centroids = dimReducedRdd.takeSample(False, quantity)
     print('sample:')
-    pp.pprint(sample)
-    sampleMean = ft.reduce(lambda a, b: a + b, sample) / quantity
-    print('sampleMean', sampleMean)
+    pp.pprint(centroids, compact=True)
+    print("Sample:", len(centroids))
 
+    # bound helpers
     tolerance = 0.2
-    upperBound = (1 + tolerance) * sampleMean
-    lowerBound = (1 - tolerance) * sampleMean
-    print('bounds [', lowerBound, ',', upperBound, ']')
+    lowerBound = 1 - tolerance
+    upperBound = 1 + tolerance
 
-    # Filter records
-    sampleRange = meansRDD.filter(lambda a: lowerBound <= a and a <= upperBound)
-    sampleAmount = sampleRange.count()
-    qualityInd = sampleAmount * workingDimsAmount
-    print('Filtered samples:', sampleAmount)
-    print('Quality ind:', qualityInd)
-    meanAfterIteration = sampleRange.reduce(lambda a, b: a + b) / sampleAmount
-    print("new mean:", meanAfterIteration)
+    def getFilterFunction(dim_boundaries):
+        def filter_by_bounds(record):
+            suspect = max(zip(record, dim_boundaries), key=lambda x: x[0])
+            dim, bounds = suspect
+            lower, upper = bounds
+            return lower < dim < upper
+        return filter_by_bounds
+
+    # amount of centroid recalculations
+    n = 3
+    for _ in range(n):
+        # calculate boundaries for each centroid, filter records
+        filteredRdds = []
+        for i in range(quantity):
+            dimBounds = [(col*lowerBound, col*upperBound) for col in centroids[i]]
+            filterByBounds = getFilterFunction(dimBounds)
+            filteredRdds.append(dimReducedRdd.filter(filterByBounds))
+
+        # for rdd in distRdds:
+        #     print(rdd, "Amount:", rdd.count())
+        #     pp.pprint(rdd.take(2), compact=True)
+        #     print()
+
+        # calculate new centroids
+        centroids = []
+        for mean, rdd in zip(centroids, filteredRdds):
+            length = rdd.count()
+            mean = rdd.reduce(lambda a, b: [(x + y) for x, y in zip(a, b)])
+            centroids.append([x/length for x in mean])
+    print(len(centroids))
+    pp.pprint(centroids)
+
+
+    # sampleMean = ft.reduce(lambda a, b: a + b, sample) / quantity
+    # print('sampleMean', sampleMean)
+    #
+    # tolerance = 0.2
+    # upperBound = (1 + tolerance) * sampleMean
+    # lowerBound = (1 - tolerance) * sampleMean
+    # print('bounds [', lowerBound, ',', upperBound, ']')
+    #
+    # # Filter records
+    # sampleRange = meansRDD.filter(lambda a: lowerBound <= a and a <= upperBound)
+    # sampleAmount = sampleRange.count()
+    # qualityInd = sampleAmount * workingDimsAmount
+    # print('Filtered samples:', sampleAmount)
+    # print('Quality ind:', qualityInd)
+    # meanAfterIteration = sampleRange.reduce(lambda a, b: a + b) / sampleAmount
+    # print("new mean:", meanAfterIteration)
 
 
 if __name__ == "__main__":
